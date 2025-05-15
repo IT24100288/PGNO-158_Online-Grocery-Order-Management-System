0068e09be10a3fce7967311293ab55cbd5301d46
Amasha sawandi <IT24103535@my.sliit.lk>
2025-05-15 14:56:56 +0530

    Welcome Servlet

---
+++ src/main/java/com/crtlcart/pgno158_onlinegroceryordermanagementsystem/servlet/WelcomeServlet.java
@@ -0,0 +1,35 @@
+package com.crtlcart.pgno158_onlinegroceryordermanagementsystem.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class WelcomeServlet package com.grocery.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class WelcomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("admin") != null) {
+            response.sendRedirect("adminDashboard.jsp");
+        } else {
+            response.sendRedirect("adminLogin.jsp");
+        }
+    }
+
+}
