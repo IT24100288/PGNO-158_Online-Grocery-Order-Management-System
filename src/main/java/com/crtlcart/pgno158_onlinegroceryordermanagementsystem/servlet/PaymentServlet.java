bcaade9dfe6d8cb043417ba08809c75ab128f4f2
IT24100715 <it24100715@my.sliit.lk>
2025-05-17 13:05:21 +0530

    import the class

--- src/main/java/com/crtlcart/pgno158_onlinegroceryordermanagementsystem/servlet/PaymentServlet.java
+++ src/main/java/com/crtlcart/pgno158_onlinegroceryordermanagementsystem/servlet/PaymentServlet.java
@@ -1,5 +1,9 @@
 package com.crtlcart.pgno158_onlinegroceryordermanagementsystem.servlet;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
